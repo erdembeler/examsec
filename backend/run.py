d879180722757c728eab9b9fc695f42e25497b93
erdembeler <erdembeler@hotmail.com>
2026-01-09 23:17:07 +0300

    Fix gunicorn entry point

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,11 @@
+# backend/run.py
 import os
 from app import create_app
 
+# Gunicorn'un bulması gereken nesne tam olarak bu 'app' değişkenidir.
 app = create_app()
 
 if __name__ == '__main__':
-    # Render'ın verdiği portu al, yoksa 5001 kullan
+    # Lokal testler için port ayarı
     port = int(os.environ.get("PORT", 5001))
-    # Host 0.0.0.0 olmalı ki dışarıdan erişilebilsin
     app.run(host='0.0.0.0', port=port)
